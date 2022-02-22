a1cd97fcac18a8f1b1a9d6f16952e41c1fa0d968
Luiz Picolo <luizpicolo@gmail.com>
2022-02-22 15:49:40 -0400

    Removido link para banco de dados

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ let connect = function() {
 
   const { Pool } = require('pg');
   const pool = new Pool({
-    connectionString: 'postgres://jbgayqih:FuFQRdsdiPJwEkCvCLB5ck9Yo2vV3YnA@kesavan.db.elephantsql.com/jbgayqih'
+    connectionString: ''
   });
 
   global.connection = pool;
